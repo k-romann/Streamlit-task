9306d34500f68709802cb1183f1c7de132c43326
k-romann <70280347+k-romann@users.noreply.github.com>
2023-01-26 09:57:54 +0300

    Add files via upload

---
+++ apple.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import yfinance as yf
+
+
+st.write("""# Company quote data
+Shown how Apple stock prices have changed!
+""")
+
+tickerSymbol = "AAPL"
+tickerData = yf.Ticker(tickerSymbol)
+tickerDf = tickerData.history(period='id', start='2008-1-01', end='2018-12-31')
+
+st.write('''
+### Price per share after the opening of trading
+''')
+st.line_chart(tickerDf.Open)
+
+st.write('''
+### Price per share after the close of trading
+''')
+st.line_chart(tickerDf.Close)
